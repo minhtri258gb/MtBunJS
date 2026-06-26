884c3b08bed1cb8668510b5a751f7537adbc093a
trinm_hinnova <trinm@hinnova.com.vn>
2026-06-26 14:58:15 +0700

    [main] tách plugin, load env

---
+++ doc/Struct.md
@@ -0,0 +1,9 @@
+
+# Core
+- BunJS: framework
+
+# LIB
+- Elysia: Server HTTP
+	> bun add elysia @elysiajs/static
+- DotENV: Load file .env
+	> bun add dotenv
---
+++ src/plugin.js
@@ -0,0 +1,40 @@
+import { readdirSync, existsSync, mkdirSync } from 'fs';
+import { join } from 'path';
+
+export default async function loadPlugins(app) {
+
+	// Tự động load các plugin .js từ thư mục /plugin
+	const pluginDir = join(process.cwd(), 'plugin');
+
+	if (!existsSync(pluginDir)) {
+		mkdirSync(pluginDir);
+	}
+
+	const files = readdirSync(pluginDir);
+
+	console.log("-----------------------------------------");
+	console.log("🚀 Đang khởi tạo hệ thống Plugin (JS)...");
+	for (const file of files) {
+
+		// Chỉ quét các file có đuôi .js
+		if (file.endsWith('.js')) {
+			const filePath = join(pluginDir, file);
+
+			try {
+				// Import động file JavaScript tại runtime
+				const module = await import(filePath);
+
+				if (typeof module.default === 'function') {
+					app.use(module.default);
+					console.log(`✅ Đã load thành công plugin JS: ${file}`);
+				} else {
+					console.log(`⚠️ Bỏ qua ${file}: Không tìm thấy 'export default function'`);
+				}
+			} catch (error) {
+				console.error(`❌ Lỗi khi load plugin ${file}:`, error);
+			}
+		}
+	}
+	console.log("-----------------------------------------");
+
+}
